b300b4a9e013d83c0e594d0aed9d848204dfb3b1
James Munns <james@onevariable.com>
2025-09-08 20:17:15 +0200

    Add ability to calculate raw header max size

--- crates/ergot/src/wire_frames.rs
+++ crates/ergot/src/wire_frames.rs
@@ -6,20 +6,24 @@ use crate::{Address, AnyAllAppendix, FrameKind, HeaderSeq, Key, ProtocolError, n
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CommonHeader {
+    // WARNING: Update MAX_HDR_ENCODED_SIZE if you add/remove anything here!
     pub src: Address,
     pub dst: Address,
     pub seq_no: u16,
     pub kind: FrameKind,
     pub ttl: u8,
+    // WARNING: Update MAX_HDR_ENCODED_SIZE if you add/remove anything here!
 }
 
 pub enum PartialDecodeTail<'a> {
+    // WARNING: Update MAX_HDR_ENCODED_SIZE if you add/remove anything here!
     Specific(&'a [u8]),
     AnyAll {
         apdx: AnyAllAppendix,
         body: &'a [u8],
     },
     Err(ProtocolError),
+    // WARNING: Update MAX_HDR_ENCODED_SIZE if you add/remove anything here!
 }
 
 pub struct PartialDecode<'a> {
@@ -97,6 +101,44 @@ impl From<postcard::Error> for EncodeFrameError {
         Self::SerializationError(value)
     }
 }
+/// The largest encoded size of a header, usable for creating a max-sized buffer
+///
+/// ```text
+/// CommonHeader=================================
+/// src: Address,            u32, varint: 5 bytes
+/// dst: Address,            u32, varint: 5 bytes
+/// seq_no: u16,             u16, varint: 3 bytes
+/// kind: FrameKind,         u8, !varint: 1 byte
+/// ttl: u8,                 u8, !varint: 1 byte
+/// AnyAllAppendix===============================
+/// key: Key,                [u8; 8]:     8 bytes
+/// nash: Option<NameHash>,  u32, varint: 5 bytes
+/// ==================================== 28 bytes
+/// ```
+//
+// TODO: A more automatic way of handling this. This is currently tested with a
+// unit test below.
+pub const MAX_HDR_ENCODED_SIZE: usize = 28;
+
+/// Encode the frame header to the given serializer
+pub fn encode_frame_hdr<F>(
+    ser: &mut Serializer<F>,
+    hdr: &CommonHeader,
+    apdx: Option<&AnyAllAppendix>,
+) -> Result<(), EncodeFrameError>
+where
+    F: ser_flavors::Flavor,
+{
+    hdr.serialize(&mut *ser)?;
+
+    if let Some(app) = apdx {
+        ser.output.try_extend(&app.key.0)?;
+        let val: u32 = app.nash.as_ref().map(NameHash::to_u32).unwrap_or(0);
+        val.serialize(ser)?;
+    }
+
+    Ok(())
+}
 
 // must not be error
 // doesn't check if dest is actually any/all
@@ -111,13 +153,7 @@ where
     T: Serialize,
 {
     let mut serializer = Serializer { output: flav };
-    hdr.serialize(&mut serializer)?;
-
-    if let Some(app) = apdx {
-        serializer.output.try_extend(&app.key.0)?;
-        let val: u32 = app.nash.as_ref().map(NameHash::to_u32).unwrap_or(0);
-        val.serialize(&mut serializer)?;
-    }
+    encode_frame_hdr(&mut serializer, hdr, apdx)?;
 
     body.serialize(&mut serializer)?;
     Ok(serializer.output.finalize()?)
@@ -182,4 +218,34 @@ pub struct BorrowedFrame<'a> {
     pub hdr: HeaderSeq,
     pub hdr_raw: &'a [u8],
     pub body: Result<&'a [u8], ProtocolError>,
+}
+
+#[cfg(all(test, feature = "std"))]
+mod test {
+    use postcard::{ser_flavors::Flavor, Serializer};
+
+    use crate::{nash::NameHash, wire_frames::MAX_HDR_ENCODED_SIZE, Address, AnyAllAppendix, FrameKind, Key};
+
+    use super::{encode_frame_hdr, CommonHeader};
+
+    #[test]
+    fn max_hdr_ser_size() {
+        let hdr = CommonHeader {
+            // Addresses: maximum integer values
+            src: Address { network_id: u16::MAX, node_id: u8::MAX, port_id: u8::MAX },
+            dst: Address { network_id: u16::MAX, node_id: u8::MAX, port_id: u8::MAX },
+            seq_no: u16::MAX,
+            kind: FrameKind(u8::MAX),
+            ttl: u8::MAX,
+        };
+        let apdx = AnyAllAppendix {
+            key: Key([0xFFu8; 8]),
+            nash: NameHash::from_u32(u32::MAX),
+        };
+        let flav = postcard::ser_flavors::StdVec::new();
+        let mut ser = Serializer { output: flav };
+        encode_frame_hdr(&mut ser, &hdr, Some(&apdx)).unwrap();
+        let res = ser.output.finalize().unwrap();
+        assert_eq!(res.len(), MAX_HDR_ENCODED_SIZE);
+    }
 }
