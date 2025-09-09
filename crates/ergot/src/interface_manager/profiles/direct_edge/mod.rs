jamesmunns/ergot

commit b23f99ed7f2d903ac37d16b1069f60db28f0f9ae

Remove `hdr_raw` usage (#134)
A retooling of how we use headers in `send_raw` operations. Major changes:

* `NetStack::send_raw` now takes `HeaderSeq` instead of `Header`
* `NetStack::send_raw` no longer takes `hdr_raw`, as well as many other parts of the system
* Reduce usage of `CommonHeader` and `AnyAllAppendix`, instead using `HeaderSeq` more consistently
* Headers are now generally re-serialized down at the InterfaceSink level, so they don't need to be re-encoded more than once.

Originally, the idea behind passing around `hdr_raw` was to avoid cases where profiles/interfaces or borrowed sockets would need to re-serialize the header when forwarding. However, there are places in the code where the header may be modified, particularly for broadcast flooding, or even for updating the TTL count when forwarding, meaning that the serialized and in-memory versions could get out of sync with each other.

To avoid this, we no longer pass around the "raw" header, and just demand it is re-serialized when necessary. This is actually likely not very burdensome at all, as the max header size is 28 bytes, which should be trivial to do.

crates/ergot/src/interface_manager/profiles/direct_edge/mod.rs
@@ -26,12 +26,12 @@ pub mod eusb_0_5;
 pub mod tokio_tcp;
 
 use crate::{
-    Header, ProtocolError,
+    Header, HeaderSeq, ProtocolError,
     interface_manager::{
         Interface, InterfaceSendError, InterfaceSink, InterfaceState, Profile, SetStateError,
     },
     net_stack::NetStackHandle,
-    wire_frames::{CommonHeader, de_frame},
+    wire_frames::de_frame,
 };
 
 pub const CENTRAL_NODE_ID: u8 = 1;
@@ -77,7 +77,7 @@ impl<I: Interface> DirectEdge<I> {
     fn common_send<'b>(
         &'b mut self,
         ihdr: &Header,
-    ) -> Result<(&'b mut I::Sink, CommonHeader), InterfaceSendError> {
+    ) -> Result<(&'b mut I::Sink, HeaderSeq), InterfaceSendError> {
         let net_id = match &self.state {
             InterfaceState::Down => {
                 trace!("{ihdr}: ignoring send, interface down");
@@ -133,16 +133,11 @@ impl<I: Interface> DirectEdge<I> {
             hdr.dst.node_id = self.other_node_id;
         }
 
-        let seq_no = self.seq_no;
-        self.seq_no = self.seq_no.wrapping_add(1);
-
-        let header = CommonHeader {
-            src: hdr.src,
-            dst: hdr.dst,
-            seq_no,
-            kind: hdr.kind,
-            ttl: hdr.ttl,
-        };
+        let header = ihdr.to_headerseq_or_with_seq(|| {
+            let seq_no = self.seq_no;
+            self.seq_no = self.seq_no.wrapping_add(1);
+            seq_no
+        });
         if [0, 255].contains(&hdr.dst.port_id) && ihdr.any_all.is_none() {
             return Err(InterfaceSendError::AnyPortMissingKey);
         }
@@ -157,7 +152,7 @@ impl<I: Interface> Profile for DirectEdge<I> {
     fn send<T: Serialize>(&mut self, hdr: &Header, data: &T) -> Result<(), InterfaceSendError> {
         let (intfc, header) = self.common_send(hdr)?;
 
-        let res = intfc.send_ty(&header, hdr.any_all.as_ref(), data);
+        let res = intfc.send_ty(&header, data);
 
         match res {
             Ok(()) => Ok(()),
@@ -186,8 +181,7 @@ impl<I: Interface> Profile for DirectEdge<I> {
 
     fn send_raw(
         &mut self,
-        _hdr: &Header,
-        _hdr_raw: &[u8],
+        _hdr: &HeaderSeq,
         _data: &[u8],
         _source: Self::InterfaceIdent,
     ) -> Result<(), InterfaceSendError> {
@@ -284,11 +278,13 @@ pub fn process_frame<N>(
     //
     // If the dest is 0, should we rewrite the dest as self.net_id? This
     // is the opposite as above, but I dunno how that will work with responses
-    let hdr = frame.hdr.clone();
-    let hdr: Header = hdr.into();
     let res = match frame.body {
-        Ok(body) => nsh.stack().send_raw(&hdr, frame.hdr_raw, body, ident),
-        Err(e) => nsh.stack().send_err(&hdr, e, Some(ident)),
+        Ok(body) => nsh.stack().send_raw(&frame.hdr, body, ident),
+        Err(e) => {
+            // send_err requires a Header instead of a HeaderSeq, so we convert it
+            let nshdr: Header = frame.hdr.clone().into();
+            nsh.stack().send_err(&nshdr, e, Some(ident))
+        }
     };
 
     match res {

crates/ergot/src/interface_manager/profiles/direct_router/mod.rs
@@ -142,8 +142,7 @@ impl<I: Interface> Profile for DirectRouter<I> {
 
     fn send_raw(
         &mut self,
-        hdr: &crate::Header,
-        hdr_raw: &[u8],
+        hdr: &crate::HeaderSeq,
         data: &[u8],
         source: Self::InterfaceIdent,
     ) -> Result<(), InterfaceSendError> {
@@ -164,10 +163,11 @@ impl<I: Interface> Profile for DirectRouter<I> {
                     continue;
                 }
 
+                // For broadcast messages, rewrite the destination address
+                // to the address of the next hop.
                 hdr.dst.network_id = p.net_id;
                 hdr.dst.node_id = EDGE_NODE_ID;
-                // TODO: this is wrong, hdr_raw and header could be out of sync!
-                any_good |= p.edge.send_raw(&hdr, hdr_raw, data).is_ok();
+                any_good |= p.edge.send_raw(&hdr, data).is_ok();
             }
             if any_good {
                 Ok(())
@@ -175,8 +175,9 @@ impl<I: Interface> Profile for DirectRouter<I> {
                 Err(InterfaceSendError::NoRouteToDest)
             }
         } else {
-            let intfc = self.find(&hdr, Some(source))?;
-            intfc.send_raw(&hdr, hdr_raw, data)
+            let nshdr = hdr.clone().into();
+            let intfc = self.find(&nshdr, Some(source))?;
+            intfc.send_raw(&hdr, data)
         }
     }
 
@@ -539,11 +540,11 @@ pub fn process_frame<N>(
         // If the dest is 0, should we rewrite the dest as self.net_id? This
         // is the opposite as above, but I dunno how that will work with responses
         let hdr = frame.hdr.clone();
-        let hdr: Header = hdr.into();
+        let nshdr: Header = hdr.clone().into();
 
         let res = match frame.body {
-            Ok(body) => nsh.stack().send_raw(&hdr, frame.hdr_raw, body, ident),
-            Err(e) => nsh.stack().send_err(&hdr, e, Some(ident)),
+            Ok(body) => nsh.stack().send_raw(&hdr, body, ident),
+            Err(e) => nsh.stack().send_err(&nshdr, e, Some(ident)),
         };
         match res {
             Ok(()) => {}
@@ -562,12 +563,11 @@ mod edge_interface_plus {
     use serde::Serialize;
 
     use crate::{
-        Header, ProtocolError,
+        Header, HeaderSeq, ProtocolError,
         interface_manager::{
             Interface, InterfaceSendError, InterfaceSink, InterfaceState, SetStateError,
             profiles::direct_edge::{CENTRAL_NODE_ID, EDGE_NODE_ID},
         },
-        wire_frames::CommonHeader,
     };
 
     // TODO: call this something like "point to point edge"
@@ -595,7 +595,7 @@ mod edge_interface_plus {
         fn common_send<'b>(
             &'b mut self,
             ihdr: &Header,
-        ) -> Result<(&'b mut I::Sink, CommonHeader), InterfaceSendError> {
+        ) -> Result<(&'b mut I::Sink, HeaderSeq), InterfaceSendError> {
             let net_id = match &self.state {
                 InterfaceState::Down | InterfaceState::Inactive => {
                     return Err(InterfaceSendError::NoRouteToDest);
@@ -636,19 +636,11 @@ mod edge_interface_plus {
             }
 
             // If this message has no seq_no, assign it one
-            let seq_no = hdr.seq_no.unwrap_or_else(|| {
+            let header = hdr.to_headerseq_or_with_seq(|| {
                 let seq_no = self.seq_no;
                 self.seq_no = self.seq_no.wrapping_add(1);
                 seq_no
             });
-
-            let header = CommonHeader {
-                src: hdr.src,
-                dst: hdr.dst,
-                seq_no,
-                kind: hdr.kind,
-                ttl: hdr.ttl,
-            };
             if [0, 255].contains(&hdr.dst.port_id) && ihdr.any_all.is_none() {
                 return Err(InterfaceSendError::AnyPortMissingKey);
             }
@@ -667,7 +659,7 @@ mod edge_interface_plus {
         ) -> Result<(), InterfaceSendError> {
             let (intfc, header) = self.common_send(hdr)?;
 
-            let res = intfc.send_ty(&header, hdr.any_all.as_ref(), data);
+            let res = intfc.send_ty(&header, data);
 
             match res {
                 Ok(()) => Ok(()),
@@ -692,15 +684,12 @@ mod edge_interface_plus {
 
         pub(super) fn send_raw(
             &mut self,
-            hdr: &Header,
-            hdr_raw: &[u8],
+            hdr: &HeaderSeq,
             data: &[u8],
         ) -> Result<(), InterfaceSendError> {
-            let (intfc, header) = self.common_send(hdr)?;
-
-            // TODO: this is wrong, hdr_raw and header could be out of sync if common_send
-            // modified the header!
-            let res = intfc.send_raw(&header, hdr_raw, data);
+            let nshdr: Header = hdr.clone().into();
+            let (intfc, header) = self.common_send(&nshdr)?;
+            let res = intfc.send_raw(&header, data);
 
             match res {
                 Ok(()) => Ok(()),

crates/ergot/src/interface_manager/mod.rs
@@ -34,7 +34,7 @@
 //!
 //! [`NetStack`]: crate::NetStack
 
-use crate::{AnyAllAppendix, Header, ProtocolError, wire_frames::CommonHeader};
+use crate::{Header, HeaderSeq, ProtocolError};
 use postcard_schema::Schema;
 use serde::{Deserialize, Serialize};
 
@@ -123,8 +123,7 @@ pub trait Profile {
     /// This method should only be used for messages that do NOT originate locally
     fn send_raw(
         &mut self,
-        hdr: &Header,
-        hdr_raw: &[u8],
+        hdr: &HeaderSeq,
         data: &[u8],
         source: Self::InterfaceIdent,
     ) -> Result<(), InterfaceSendError>;
@@ -182,14 +181,9 @@ pub trait Interface {
 /// TX worker.
 #[allow(clippy::result_unit_err)]
 pub trait InterfaceSink {
-    fn send_ty<T: Serialize>(
-        &mut self,
-        hdr: &CommonHeader,
-        apdx: Option<&AnyAllAppendix>,
-        body: &T,
-    ) -> Result<(), ()>;
-    fn send_raw(&mut self, hdr: &CommonHeader, hdr_raw: &[u8], body: &[u8]) -> Result<(), ()>;
-    fn send_err(&mut self, hdr: &CommonHeader, err: ProtocolError) -> Result<(), ()>;
+    fn send_ty<T: Serialize>(&mut self, hdr: &HeaderSeq, body: &T) -> Result<(), ()>;
+    fn send_raw(&mut self, hdr: &HeaderSeq, body: &[u8]) -> Result<(), ()>;
+    fn send_err(&mut self, hdr: &HeaderSeq, err: ProtocolError) -> Result<(), ()>;
 }
 
 #[cfg_attr(feature = "defmt-v1", derive(defmt::Format))]
@@ -203,8 +197,8 @@ pub enum InterfaceSendError {
     /// Profile found a destination interface, but that interface
     /// was full in space/slots
     InterfaceFull,
-    /// TODO: Remove
-    PlaceholderOhNo,
+    /// An unhandled internal error occurred, this is a bug.
+    InternalError,
     /// Destination was an "any" port, but a key was not provided
     AnyPortMissingKey,
     /// TTL has reached the terminal value
@@ -251,7 +245,7 @@ impl InterfaceSendError {
             InterfaceSendError::DestinationLocal => ProtocolError::ISE_DESTINATION_LOCAL,
             InterfaceSendError::NoRouteToDest => ProtocolError::ISE_NO_ROUTE_TO_DEST,
             InterfaceSendError::InterfaceFull => ProtocolError::ISE_INTERFACE_FULL,
-            InterfaceSendError::PlaceholderOhNo => ProtocolError::ISE_PLACEHOLDER_OH_NO,
+            InterfaceSendError::InternalError => ProtocolError::ISE_INTERNAL_ERROR,
             InterfaceSendError::AnyPortMissingKey => ProtocolError::ISE_ANY_PORT_MISSING_KEY,
             InterfaceSendError::TtlExpired => ProtocolError::ISE_TTL_EXPIRED,
             InterfaceSendError::RoutingLoop => ProtocolError::ISE_ROUTING_LOOP,

crates/ergot/src/net_stack/mod.rs
@@ -27,7 +27,7 @@ use serde::Serialize;
 use topics::Topics;
 
 use crate::{
-    Header, ProtocolError,
+    Header, HeaderSeq, ProtocolError,
     fmtlog::{ErgotFmtTx, Level},
     interface_manager::{self, InterfaceSendError, Profile},
     socket::{SocketHeader, SocketSendError},
@@ -209,13 +209,12 @@ where
     /// [`NetStack`].
     pub fn send_raw(
         &self,
-        hdr: &Header,
-        hdr_raw: &[u8],
+        hdr: &HeaderSeq,
         body: &[u8],
         source: P::InterfaceIdent,
     ) -> Result<(), NetStackSendError> {
         self.inner
-            .try_with_lock(|inner| inner.send_raw(hdr, hdr_raw, body, source))
+            .try_with_lock(|inner| inner.send_raw(hdr, body, source))
             .ok_or(NetStackSendError::WouldDeadlock)?
     }
 

crates/ergot/src/socket/mod.rs
@@ -58,11 +58,7 @@ use core::{
     ptr::{self, NonNull},
 };
 
-use crate::{
-    FrameKind, HeaderSeq, Key, ProtocolError,
-    nash::NameHash,
-    wire_frames::{self, CommonHeader},
-};
+use crate::{FrameKind, HeaderSeq, Key, ProtocolError, nash::NameHash, wire_frames};
 use cordyceps::{Linked, list::Links};
 use postcard::ser_flavors;
 use serde::Serialize;
@@ -159,8 +155,6 @@ pub type RecvRaw = fn(
     &[u8],
     // the header
     HeaderSeq,
-    // the raw header
-    &[u8],
 ) -> Result<(), SocketSendError>;
 
 pub type RecvError = fn(
@@ -181,15 +175,7 @@ pub(crate) fn borser<T: Serialize>(
     let that: &T = unsafe { that.as_ref() };
     let ser = ser_flavors::Slice::new(out);
 
-    let chdr = CommonHeader {
-        src: hdr.src,
-        dst: hdr.dst,
-        seq_no: hdr.seq_no,
-        kind: hdr.kind,
-        ttl: hdr.ttl,
-    };
-
-    let Ok(used) = wire_frames::encode_frame_ty(ser, &chdr, hdr.any_all.as_ref(), that) else {
+    let Ok(used) = wire_frames::encode_frame_ty(ser, &hdr, that) else {
         log::trace!("BOOP");
         return Err(SocketSendError::NoSpace);
     };

crates/ergot/src/conformance/net_stack.rs
@@ -103,7 +103,7 @@ pub mod mocks {
     use mutex::raw_impls::cs::CriticalSectionRawMutex;
 
     use crate::{
-        Header, ProtocolError,
+        Header, HeaderSeq, ProtocolError,
         interface_manager::{InterfaceSendError, InterfaceState, Profile, SetStateError},
         net_stack::ArcNetStack,
     };
@@ -126,8 +126,7 @@ pub mod mocks {
     }
 
     pub struct ExpectedSendRaw {
-        pub hdr: Header,
-        pub hdr_raw: Vec<u8>,
+        pub hdr: HeaderSeq,
         pub body: Vec<u8>,
         pub retval: Result<(), InterfaceSendError>,
     }
@@ -186,8 +185,7 @@ pub mod mocks {
 
         fn send_raw(
             &mut self,
-            _hdr: &Header,
-            _hdr_raw: &[u8],
+            _hdr: &HeaderSeq,
             _data: &[u8],
             _source: Self::InterfaceIdent,
         ) -> Result<(), InterfaceSendError> {

crates/ergot/src/interface_manager/profiles/null.rs
@@ -7,7 +7,7 @@
 use serde::Serialize;
 
 use crate::{
-    Header,
+    Header, HeaderSeq,
     interface_manager::{ConstInit, InterfaceSendError, InterfaceState, Profile, SetStateError},
 };
 
@@ -33,8 +33,7 @@ impl Profile for Null {
 
     fn send_raw(
         &mut self,
-        hdr: &Header,
-        _hdr_raw: &[u8],
+        hdr: &HeaderSeq,
         _data: &[u8],
         _source: Self::InterfaceIdent,
     ) -> Result<(), InterfaceSendError> {

crates/ergot/src/interface_manager/utils/cobs_stream.rs
@@ -4,13 +4,16 @@
 //! interfaces that require framing in software.
 
 use bbq2::{prod_cons::stream::StreamProducer, traits::bbqhdl::BbqHandle};
-use postcard::ser_flavors::{self, Flavor};
+use postcard::{
+    Serializer,
+    ser_flavors::{self, Flavor},
+};
 use serde::Serialize;
 
 use crate::{
-    AnyAllAppendix, FrameKind, ProtocolError,
+    FrameKind, HeaderSeq, ProtocolError,
     interface_manager::InterfaceSink,
-    wire_frames::{self, CommonHeader},
+    wire_frames::{self, MAX_HDR_ENCODED_SIZE, encode_frame_hdr},
 };
 
 pub struct Sink<Q>
@@ -43,12 +46,7 @@ impl<Q> InterfaceSink for Sink<Q>
 where
     Q: BbqHandle,
 {
-    fn send_ty<T: Serialize>(
-        &mut self,
-        hdr: &CommonHeader,
-        apdx: Option<&AnyAllAppendix>,
-        body: &T,
-    ) -> Result<(), ()> {
+    fn send_ty<T: Serialize>(&mut self, hdr: &HeaderSeq, body: &T) -> Result<(), ()> {
         let is_err = hdr.kind == FrameKind::PROTOCOL_ERROR;
 
         if is_err {
@@ -60,36 +58,36 @@ where
         let mut wgr = self.prod.grant_exact(max_len).map_err(drop)?;
 
         let ser = ser_flavors::Cobs::try_new(ser_flavors::Slice::new(&mut wgr)).map_err(drop)?;
-        let used = wire_frames::encode_frame_ty(ser, hdr, apdx, body).map_err(drop)?;
+        let used = wire_frames::encode_frame_ty(ser, hdr, body).map_err(drop)?;
         let len = used.len();
         wgr.commit(len);
 
         Ok(())
     }
 
-    fn send_raw(&mut self, hdr: &CommonHeader, hdr_raw: &[u8], body: &[u8]) -> Result<(), ()> {
+    fn send_raw(&mut self, hdr: &HeaderSeq, body: &[u8]) -> Result<(), ()> {
         let is_err = hdr.kind == FrameKind::PROTOCOL_ERROR;
 
         if is_err {
             // todo: use a different interface for this
             return Err(());
         }
-
-        let max_len = cobs::max_encoding_length(hdr_raw.len() + body.len());
+        let max_len = cobs::max_encoding_length(MAX_HDR_ENCODED_SIZE + body.len());
         let mut wgr = self.prod.grant_exact(max_len).map_err(drop)?;
 
-        let mut ser =
-            ser_flavors::Cobs::try_new(ser_flavors::Slice::new(&mut wgr)).map_err(drop)?;
-        ser.try_extend(hdr_raw).map_err(drop)?;
-        ser.try_extend(body).map_err(drop)?;
-        let fin = ser.finalize().map_err(drop)?;
+        let mut ser = Serializer {
+            output: ser_flavors::Cobs::try_new(ser_flavors::Slice::new(&mut wgr)).map_err(drop)?,
+        };
+        encode_frame_hdr(&mut ser, hdr).map_err(drop)?;
+        ser.output.try_extend(body).map_err(drop)?;
+        let fin = ser.output.finalize().map_err(drop)?;
         let len = fin.len();
         wgr.commit(len);
 
         Ok(())
     }
 
-    fn send_err(&mut self, hdr: &CommonHeader, err: ProtocolError) -> Result<(), ()> {
+    fn send_err(&mut self, hdr: &HeaderSeq, err: ProtocolError) -> Result<(), ()> {
         let is_err = hdr.kind == FrameKind::PROTOCOL_ERROR;
 
         // note: here it SHOULD be an err!

commit ceb6f37621d4e284fd87c6fa00adbfab8adce930

Shadow the header to avoid using the wrong one
fixes a regression in #134

crates/ergot/src/interface_manager/profiles/direct_edge/mod.rs
@@ -76,26 +76,26 @@ impl<I: Interface> DirectEdge<I> {
 impl<I: Interface> DirectEdge<I> {
     fn common_send<'b>(
         &'b mut self,
-        ihdr: &Header,
+        hdr: &Header,
     ) -> Result<(&'b mut I::Sink, HeaderSeq), InterfaceSendError> {
         let net_id = match &self.state {
             InterfaceState::Down => {
-                trace!("{ihdr}: ignoring send, interface down");
+                trace!("{hdr}: ignoring send, interface down");
                 return Err(InterfaceSendError::NoRouteToDest);
             }
             InterfaceState::Inactive => {
-                trace!("{ihdr}: ignoring send, interface inactive");
+                trace!("{hdr}: ignoring send, interface inactive");
                 return Err(InterfaceSendError::NoRouteToDest);
             }
             InterfaceState::ActiveLocal { .. } => {
                 // TODO: maybe also handle this?
-                trace!("{ihdr}: ignoring send, interface local only");
+                trace!("{hdr}: ignoring send, interface local only");
                 return Err(InterfaceSendError::NoRouteToDest);
             }
             InterfaceState::Active { net_id, node_id: _ } => *net_id,
         };
 
-        trace!("{ihdr}: common_send");
+        trace!("{hdr}: common_send");
 
         if net_id == 0 {
             debug!("Attempted to send via interface before we have been assigned a net ID");
@@ -107,13 +107,13 @@ impl<I: Interface> DirectEdge<I> {
 
         // TODO: a LOT of this is copy/pasted from the router, can we make this
         // shared logic, or handled by the stack somehow?
-        if ihdr.dst.network_id == net_id && ihdr.dst.node_id == self.own_node_id {
+        if hdr.dst.network_id == net_id && hdr.dst.node_id == self.own_node_id {
             return Err(InterfaceSendError::DestinationLocal);
         }
 
         // Now that we've filtered out "dest local" checks, see if there is
         // any TTL left before we send to the next hop
-        let mut hdr = ihdr.clone();
+        let mut hdr = hdr.clone();
         hdr.decrement_ttl()?;
 
         // If the source is local, rewrite the source using this interface's
@@ -133,12 +133,12 @@ impl<I: Interface> DirectEdge<I> {
             hdr.dst.node_id = self.other_node_id;
         }
 
-        let header = ihdr.to_headerseq_or_with_seq(|| {
+        let header = hdr.to_headerseq_or_with_seq(|| {
             let seq_no = self.seq_no;
             self.seq_no = self.seq_no.wrapping_add(1);
             seq_no
         });
-        if [0, 255].contains(&hdr.dst.port_id) && ihdr.any_all.is_none() {
+        if [0, 255].contains(&hdr.dst.port_id) && hdr.any_all.is_none() {
             return Err(InterfaceSendError::AnyPortMissingKey);
         }
 

crates/ergot/src/interface_manager/profiles/direct_router/mod.rs
@@ -314,16 +314,16 @@ impl<I: Interface> DirectRouter<I> {
 
     fn find<'b>(
         &'b mut self,
-        ihdr: &Header,
+        hdr: &Header,
         source: Option<<Self as Profile>::InterfaceIdent>,
     ) -> Result<&'b mut edge_interface_plus::EdgeInterfacePlus<I>, InterfaceSendError> {
         // todo: make this state impossible? enum of dst w/ or w/o key?
-        if ihdr.dst.port_id == 0 && ihdr.any_all.is_none() {
+        if hdr.dst.port_id == 0 && hdr.any_all.is_none() {
             return Err(InterfaceSendError::AnyPortMissingKey);
         }
 
         // Find destination by net_id
-        let Some(rte) = self.routes.get_mut(&ihdr.dst.network_id) else {
+        let Some(rte) = self.routes.get_mut(&hdr.dst.network_id) else {
             return Err(InterfaceSendError::NoRouteToDest);
         };
 
@@ -335,7 +335,7 @@ impl<I: Interface> DirectRouter<I> {
             } => {
                 let now = Instant::now();
                 if expiration_time <= now {
-                    warn!("Tombstoning net_id: {}", ihdr.dst.network_id);
+                    warn!("Tombstoning net_id: {}", hdr.dst.network_id);
                     rte.kind = RouteKind::Tombstone {
                         clear_time: now + Duration::from_secs(30),
                     };
@@ -346,7 +346,7 @@ impl<I: Interface> DirectRouter<I> {
                 let now = Instant::now();
                 if clear_time <= now {
                     // times up, get gone.
-                    self.routes.remove(&ihdr.dst.network_id);
+                    self.routes.remove(&hdr.dst.network_id);
                 }
                 return Err(InterfaceSendError::NoRouteToDest);
             }
@@ -358,14 +358,14 @@ impl<I: Interface> DirectRouter<I> {
             //  associated with it. Remove the route, return no route.
             warn!(
                 "Stale route with net_id: {}, ident: {}, removing",
-                ihdr.dst.network_id, rte.ident
+                hdr.dst.network_id, rte.ident
             );
-            self.routes.remove(&ihdr.dst.network_id);
+            self.routes.remove(&hdr.dst.network_id);
             return Err(InterfaceSendError::NoRouteToDest);
         };
 
         // Is this actually for us?
-        if (ihdr.dst.network_id == intfc.net_id) && (ihdr.dst.node_id == CENTRAL_NODE_ID) {
+        if (hdr.dst.network_id == intfc.net_id) && (hdr.dst.node_id == CENTRAL_NODE_ID) {
             return Err(InterfaceSendError::DestinationLocal);
         }
 
@@ -529,8 +529,21 @@ pub fn process_frame<N>(
         // we should rewrite it so it isn't later understood as a
         // local packet.
         if frame.hdr.src.network_id == 0 {
-            assert_ne!(frame.hdr.src.node_id, 0, "we got a local packet remotely?");
-            assert_ne!(frame.hdr.src.node_id, 1, "someone is pretending to be us?");
+            match frame.hdr.src.node_id {
+                0 => {
+                    log::warn!("{}: device is sending us frames without a node id, ignoring", frame.hdr);
+                    return;
+                }
+                CENTRAL_NODE_ID => {
+                    log::warn!("{}: device is sending us frames as us, ignoring", frame.hdr);
+                    return;
+                }
+                EDGE_NODE_ID => {}
+                _ => {
+                    log::warn!("{}: device is sending us frames with a bad node id, ignoring", frame.hdr);
+                    return;
+                }
+            }
 
             frame.hdr.src.network_id = net_id;
         }
@@ -594,7 +607,7 @@ mod edge_interface_plus {
     impl<I: Interface> EdgeInterfacePlus<I> {
         fn common_send<'b>(
             &'b mut self,
-            ihdr: &Header,
+            hdr: &Header,
         ) -> Result<(&'b mut I::Sink, HeaderSeq), InterfaceSendError> {
             let net_id = match &self.state {
                 InterfaceState::Down | InterfaceState::Inactive => {
@@ -607,16 +620,16 @@ mod edge_interface_plus {
                 InterfaceState::Active { net_id, node_id: _ } => *net_id,
             };
 
-            trace!("{ihdr} common_send");
+            trace!("{hdr} common_send");
 
             // TODO: when this WAS a real Profile, we did a lot of these things, but
             // now they should be done by the router. For now, we just have asserts,
             // eventually we should relax this to debug_asserts?
             assert!(net_id != 0);
-            let for_us = ihdr.dst.network_id == net_id && ihdr.dst.node_id == self.own_node_id;
+            let for_us = hdr.dst.network_id == net_id && hdr.dst.node_id == self.own_node_id;
             assert!(!for_us);
 
-            let mut hdr = ihdr.clone();
+            let mut hdr = hdr.clone();
 
             // If the source is local, rewrite the source using this interface's
             // information so responses can find their way back here
@@ -641,7 +654,7 @@ mod edge_interface_plus {
                 self.seq_no = self.seq_no.wrapping_add(1);
                 seq_no
             });
-            if [0, 255].contains(&hdr.dst.port_id) && ihdr.any_all.is_none() {
+            if [0, 255].contains(&hdr.dst.port_id) && hdr.any_all.is_none() {
                 return Err(InterfaceSendError::AnyPortMissingKey);
             }
 